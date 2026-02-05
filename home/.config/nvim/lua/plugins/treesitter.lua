0f2e24ee7dd746a8c00822b8195958a9d8cba329
itsfabijano <fabian.ehringer@gmx.net>
2026-02-05 01:11:17 +0000

    revert back to old treesitter because of compativility issues

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,9 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
+		branch = "master",
 		config = function()
-			local treesitter = require("nvim-treesitter")
-			treesitter.setup({
+			require("nvim-treesitter.configs").setup({
 				-- A list of parser names, or "all"
 				ensure_installed = {
 					"vimdoc",
@@ -15,7 +14,7 @@ return {
 					"rust",
 					"jsdoc",
 					"bash",
-					"graphql",
+					"go",
 				},
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
@@ -32,6 +31,23 @@ return {
 				highlight = {
 					-- `false` will disable the whole extension
 					enable = true,
+					disable = function(lang, buf)
+						if lang == "html" then
+							print("disabled")
+							return true
+						end
+
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							vim.notify(
+								"File larger than 100KB treesitter disabled for performance",
+								vim.log.levels.WARN,
+								{ title = "Treesitter" }
+							)
+							return true
+						end
+					end,
 
 					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 					-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
@@ -40,16 +56,38 @@ return {
 					additional_vim_regex_highlighting = { "markdown", "graphql" },
 				},
 			})
+
+			local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			treesitter_parser_config.templ = {
+				install_info = {
+					url = "https://github.com/vrischmann/tree-sitter-templ.git",
+					files = { "src/parser.c", "src/scanner.c" },
+					branch = "master",
+				},
+			}
+
+			vim.treesitter.language.register("templ", "templ")
 		end,
 	},
+
 	{
 		"nvim-treesitter/nvim-treesitter-context",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
+		after = "nvim-treesitter",
 		config = function()
 			require("treesitter-context").setup({
-				enable = true,
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				multiwindow = false, -- Enable multiwindow support.
+				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+				line_numbers = true,
+				multiline_threshold = 20, -- Maximum number of lines to show for a single context
+				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				-- Separator between context and content. Should be a single character string, like '-'.
+				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+				separator = nil,
+				zindex = 20, -- The Z-index of the context window
+				on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 			})
 		end,
 	},
